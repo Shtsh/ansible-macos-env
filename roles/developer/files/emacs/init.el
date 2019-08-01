9e9f248e54a0c7f81140049fd9df03c2df120fd3
Anton Liparin <a_liparin@wargaming.net>
2019-08-01 15:23:03 -0700

    Restart emacs using handlers

--- roles/developer/files/emacs/init.el
+++ roles/developer/files/emacs/init.el
@@ -6,8 +6,7 @@
 
 (require 'package)
 
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")))
 
 (package-initialize)
 (when (not package-archive-contents)
